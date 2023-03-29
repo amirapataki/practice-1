7984d23d39a28e5dedbaaef471f10870021a26dd
Amira <kenenbaeva01@gmail.com>
2023-03-29 19:53:37 +0600

    сделала практику

---
+++ bin/real_lesson_2.dart
@@ -0,0 +1,76 @@
+
+
+
+import 'package:real_lesson_2/real_lesson_2.dart' as real_lesson_2;
+
+void main() {
+  // int a = 10;
+  // int b = 20;
+  // int c = a + b;
+  // print (a+c);
+
+//  String msg = 'Добро пожаловать во Flutter';
+//  String msg1 ="Добро пожаловать во Флатер";
+//  print (msg);
+//  print (msg1); 
+
+//  String sayHello = 'Hello World';
+//  print(sayHello);
+
+//  String sl = '123';
+//  String sl2 = '321';
+//  print(sl+sl2);
+
+//  bool isValid = true;
+//  isValid = false;
+//  print(isValid);
+
+
+//  List array = [1,2,3,'okey'];
+//  print(array[3]);
+
+//  Map student = {
+//   'name ': "amira", 
+//   'age': 22,
+//   'isFinishedStudy' : false,
+//   };
+//   print(student ["name"]);
+
+//   dynamic number = 10 ;
+//   print(number);
+
+//   final a;
+//   a = 20;
+  
+ int f = 89;
+
+print(f);
+ 
+double o = 2.67;
+print(o);
+
+String like = 'moana';
+print(like);
+
+List real = [1,5,6,7,8,9,5,4];
+print(real);
+
+Map worker = {
+  'stage': "director",
+};
+print(worker);
+
+
+final a;
+a = 34;
+print(a);
+
+const r = 'hg';
+print (r);
+
+num n = 98;
+num m = 10/2;
+print(m);
+print(n);
+
+}
---
+++ test/real_lesson_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:real_lesson_2/real_lesson_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
